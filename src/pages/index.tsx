f87099002289cc12b22caa857337474191da4c21
shitaro <apprentice.physicist.shitaro@gmail.com>
2020-01-20 02:28:21 +0900

    Remove codes commented out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,57 +5,11 @@
 
 import React from "react";
 import { NextPage } from "next";
-// import Link from "next/link";
 import Head from "next/head";
 import MediaCard from "../components/MediaCard";
-// import StyledComponent from "../components/index";
-
 import articleMetaList from "../utils/ArticleMetaList";
-// import data from "../components/article-list.json"
-
-// const articleList = data["article"];
-
-
-// const articleComponentList = () => {
-//   const sortedArticleList = SortArticleByDate(MakePairOfPostDate(articleList));
-//   console.log(sortedArticleList);
-//   return (
-//     sortedArticleList.map(article => {
-//       console.log(article[0])
-//       const { meta } = require(`../pages/articles/${article[0]}.mdx`);
-
-//       return (
-//         <>
-//           <Link href={`/articles/${article[0]}`} as={`/post/${article[0]}`}>
-//             <a>{meta.title}</a>
-//           </Link>
-//           <p>{meta.description}</p>
-//         </>
-//       )
-//     })
-//   )
-// }
-
-// const articleComponentList: JSX.Element[] = articleList.map(article => {
-//   const { meta } = require(`../pages/articles/${article}.mdx`);
-
-//   return (
-//     <>
-//       <Link href={`/articles/${article}`} as={`/post/${article}`}>
-//         <a>{meta.title}</a>
-//       </Link>
-//       <p>{meta.description}</p>
-//     </>
-//   )
-// })
 
 const IndexPage: NextPage = () => {
-  // var articleMetaList = getArticleMetaList(articleList);
-  // console.log(articleMetaList[0]);
-  // console.log(articleMetaList[1]);
-  // const hoge = articleMetaList[0];
-  // console.log(Object.keys(hoge)[0])
-  // console.log(hoge["bonjour"].title)
   articleMetaList.sort((a,b) => {
     const keyA = Object.keys(a)[0];
     const keyB = Object.keys(b)[0];
@@ -63,11 +17,6 @@ const IndexPage: NextPage = () => {
       a[keyA].title < b[keyB].title ? 1 : -1
     )
   });
-  // console.log(articleMetaList[0]);
-  // console.log(articleMetaList[1]);
-  // const sortedArticleMetaList = articleMetaList.sort((a,b)=> a.postDate > b.postDate ? 1 : -1 );
-  // console.log(sortedArticleMetaList[0]);
-  // console.log(sortedArticleMetaList[1]);
 
   return (
     <>
@@ -87,23 +36,6 @@ const IndexPage: NextPage = () => {
           />
         )
       })}
-      {/* {articleList.map(article => {
-        const { meta } = require(`../pages/articles/${article}.mdx`);
-        return (
-          <>
-            <MediaCard
-              article={article}
-              title={meta.title}
-              thumbnail={meta.thumbnail}
-              description={meta.description}
-              categoryList={meta.category}
-            />
-          </>
-        )
-      })} */}
-      {/* <StyledComponent />
-      <h1>Article List</h1>
-      {articleComponentList} */}
     </>
   )
 };
