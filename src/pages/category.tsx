51022d8186511c308ca39416c7b86badf217b6ac
shintaroRC <rightwriting524@gmail.com>
2020-01-22 01:56:31 +0900

    refactor: use new articleList in category.tsx

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -7,7 +7,7 @@ import React from "react";
 import { NextPage } from "next";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import articleList from "../components/article-list.json";
+import { articleList } from "./articles/_articleList";
 
 const CategoryPage: NextPage = () => {
     let categories: string[] = [];
