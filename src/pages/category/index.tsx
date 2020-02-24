2c16bc71d232a17b1057bbc934557de8ad12c812
shitaro <apprentice.physicist.shitaro@gmail.com>
2020-02-24 17:34:42 +0900

    Move the file corresponding to the category top page

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -7,10 +7,10 @@ import React from "react";
 import { NextPage } from "next";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import categoryList from "../assets/categoryList";
+import categoryList from "../../assets/categoryList";
 import List from "@material-ui/core/List";
 import ListItemText from "@material-ui/core/ListItemText";
-import ListItemLink from "../components/atoms/ListItemLink";
+import ListItemLink from "../../components/atoms/ListItemLink";
 
 const CategoryPage: NextPage = () => {
     return (
